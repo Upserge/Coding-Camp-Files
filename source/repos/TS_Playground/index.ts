939bf28f8d2c7e005a90bf66ccdf969cf6451072
Jason Salas <upserge@gmail.com>
2026-01-17 09:59:27 -0800

    Add TS/angular/firebase projects

--- source/repos/TS_Playground/index.ts
+++ source/repos/TS_Playground/index.ts
@@ -1,6 +1,11 @@
-﻿// To run this, enter into terminal: npx ts-node index.ts 
+// To run this, enter into terminal: npx ts-node index.ts 
 
 console.log("TypeScript works inside VS Code");
 var x: string = "Hello";
 
+function greet(name: string): string { 
+   return x + ", " + name + "!";
+};
+
+greet("Jason");
 console.log(x);
